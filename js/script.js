217b99c71b09df0d34d70b7115abfa1a00cc44f5
tanbirahmadfr <018tanbirahmad@gmail.com>
2023-01-29 22:12:42 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ $('.counter').counterUp();
   });
   // ========back to top part ======
    // =========sticky menu part ========
-var main_menu = document.getElementById("main_menu");
+var mainMenu = document.getElementById("main_menu");
 window.addEventListener("scroll",function(){
-main_menu.classList.toggle("sticky",window.scrollY > 100);
+mainMenu.classList.toggle("sticky",window.scrollY > 100);
 })
 // =========sticky menu part ========
